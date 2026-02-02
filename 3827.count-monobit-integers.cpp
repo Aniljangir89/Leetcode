5d951fe57c6f1395c9d631be2e1a0f5a5faf4913
AS-MAC-1336 <as-mac-1336@AS-MAC-1336s-MacBook-Air.local>
2026-02-02 18:43:49 +0530

    added Leetcode:3827

---
+++ 3827.count-monobit-integers.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=3827 lang=cpp
+ *
+ * [3827] Count Monobit Integers
+ */
+
+// @lc code=start
+class Solution {
+public:
+
+    bool f(int num,int bit){
+        string str = "";
+        while(num){
+            int is = num%2;
+            num/=2;
+            if(is!=bit)return false;
+        }
+        return true;
+
+    }
+    int countMonobit(int n) {
+        int ans =0 ;
+        while(n>=0){
+            if(f(n,0) || f(n,1)) ans++;
+            n--;
+
+        }
+        return ans;
+    }
+    
+};
+// @lc code=end
+
