026e4322aca14d45c7558322b529e5a3fc3decab
AS-MAC-1336 <as-mac-1336@AS-MAC-1336s-MacBook-Air.local>
2026-02-17 14:48:50 +0530

    added Leetcode : 64

---
+++ 64.minimum-path-sum.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=64 lang=cpp
+ *
+ * [64] Minimum Path Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int f(auto & dp,auto &nums,int i,int j){
+        int n = nums.size();
+        int m = nums[0].size();
+        if(i>=n || j>=m)return INT_MAX/10;
+        if(i==n-1 && j==m-1)return nums[i][j];
+        if(dp[i][j]!=-1)return dp[i][j];
+        return dp[i][j] = nums[i][j]+min(f(dp,nums,i+1,j),f(dp,nums,i,j+1));
+    }
+    int minPathSum(vector<vector<int>>& nums) {
+        int n = nums.size();
+        int m = nums[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(dp,nums,0,0);
+    }
+};
+// @lc code=end
+
